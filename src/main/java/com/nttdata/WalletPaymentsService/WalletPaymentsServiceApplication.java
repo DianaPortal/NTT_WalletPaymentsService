af1abaa79a7c7c3e421cedb62c38a18f2d03d0cd
Diana <portal98d@gmail.com>
2025-09-17 14:07:49 -0500

    Initial commit

---
+++ src/main/java/com/nttdata/WalletPaymentsService/WalletPaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.WalletPaymentsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletPaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletPaymentsServiceApplication.class, args);
+	}
+
+}
